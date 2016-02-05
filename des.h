f61991d6b8ccddf2f5f007a4e5c42f0274bc880b
Dennis Tu <jq2tu@uwaterloo.ca>
2016-02-04 22:53:46 -0500

    latest

--- des.h
+++ des.h
@@ -3,13 +3,13 @@ using namespace std;
 class SimResult{
   public:
     double avg_pckts_in_sys;
-    
+
     SimResult(double avg_pckts_in_sys){
       this->avg_pckts_in_sys = avg_pckts_in_sys;
     };
 };
 
-std::ostream & operator<<(std::ostream & Str, const SimResult& result) { 
+std::ostream & operator<<(std::ostream & Str, const SimResult& result) {
   Str<<"average packets in the system: "<<result.avg_pckts_in_sys<<endl;
   return Str;
 }
@@ -23,7 +23,7 @@ class DES{
     int idle_count = 0;
     int drops = 0;
     int gen = 0;
-    
+
     void simulate_finite_queue(EventList* events, int link_rate, default_random_engine generator, exponential_distribution<double> packet_length_distribution, int queue_size){
       //finite queue
       double start, next_available = 0;
@@ -39,7 +39,7 @@ class DES{
             } else {
               this->Na += 1;
               this->Nsys += 1;
-              //schedule departure 
+              //schedule departure
               start = max(temp->event->time, next_available);
               L = round(packet_length_distribution(generator));
               //cout << " packet length is "<<L<<" bits";
@@ -62,7 +62,7 @@ class DES{
         temp = temp->next;
       }
     };
-    
+
     SimResult* simulate_infinite_queue(EventList* events, int link_rate, default_random_engine generator, exponential_distribution<double> packet_length_distribution){
               cout << "hi" << endl;
           cout.flush();
@@ -78,15 +78,15 @@ class DES{
           cout.flush();
           mark += 10;
         }
-        //cout<<*(temp->event);
+        cout<<*(temp->event);
         switch(temp->event->event_type){
           case Arrival:
             this->Na += 1;
-            this->Nsys += 1; 
-            //schedule departure 
+            this->Nsys += 1;
+            //schedule departure
             start = max(temp->event->time, next_available);
             L = round(packet_length_distribution(generator));
-            //cout << " packet length is "<<L<<" bits";
+            cout << " packet length is "<<L<<" bits";
             next_available = start + L/(double)link_rate;
             events->put_and_sort(new Event(next_available, Departure));
             break;
@@ -102,7 +102,7 @@ class DES{
             }
             break;
         }
-        //cout<<endl;
+        cout<<endl;
         temp = temp->next;
       }
       return new SimResult(((double)pckts_in_sys_sum)/No);
--- sim.c
+++ sim.c
@@ -8,7 +8,7 @@
 using namespace std;
 
 void test_exp_dist(default_random_engine generator, exponential_distribution<double> distribution, double lambda, int trials){
-  double sum = 0; 
+  double sum = 0;
 
   for(int i = 0; i<trials; i++){
     sum += distribution(generator);
@@ -29,7 +29,7 @@ EventList* generate_event_list(double lambda, double T, EventType type, default_
 
 EventList* generate_departure_events(EventList* arrivals, int link_rate, default_random_engine generator, exponential_distribution<double> packet_length_distribution){
   //assuming infinite queue
-  
+
   EventList* result = new EventList();
   double next_finish = 0;
   double start;
@@ -52,13 +52,13 @@ int main()
   int average_packet_length = 12000;
   int link_rate = 1000000;
   double lambda = rho*link_rate/(double)average_packet_length;
-  
+
   int queue_size = 100000;
   default_random_engine generator(random_device{}());
   exponential_distribution<double> distribution(lambda);
   exponential_distribution<double> packet_length_distribution(1l/(double)average_packet_length);
   test_exp_dist(generator, distribution, lambda, 1000000);
-  double T = 1000;
+  double T = 3;
   //generate arrival events
   EventList* arrivals = generate_event_list(lambda, T, Arrival, generator, distribution);
   //generate departure events
@@ -75,5 +75,3 @@ int main()
   cout<<(*result)<<endl;
   return 0;
 }
-
-
